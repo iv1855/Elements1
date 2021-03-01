6520748027651dfeb72a34555c0a04b07b4cdb37
i <rp893@yandex.ru>
2021-03-01 17:59:27 +0300

    2 comm

--- app/src/main/java/com/example/elements1/MainActivity.java
+++ app/src/main/java/com/example/elements1/MainActivity.java
@@ -2,7 +2,9 @@ package com.example.elements1;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void showSettings(View view) {
+        Intent intent = new Intent(this, SettingsActivity.class);
+        startActivity(intent);
+   }
+
+    public void showResult(View view) {
+        Intent intent = new Intent(this, ResultActivity.class);
+        startActivity(intent);
     }
 }
