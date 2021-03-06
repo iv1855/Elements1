45f6bfd5b4d99b395564b928a653ea8f27201bae
i <rp893@yandex.ru>
2021-03-06 21:28:27 +0300

    10 comm

--- app/src/main/java/com/example/elements1/ResultActivity.java
+++ app/src/main/java/com/example/elements1/ResultActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class ResultActivity extends AppCompatActivity {
 
@@ -12,6 +13,8 @@ public class ResultActivity extends AppCompatActivity {
     private int qty_H;
     private int qty_V;
 
+    private TextView txtViewAllData;
+
 
 
     @Override
@@ -19,15 +22,21 @@ public class ResultActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_result);
 
+        txtViewAllData = findViewById(R.id.txtView_AllData);
+
         Intent intent = getIntent();
         if (intent.hasExtra(getString(R.string.key_QtyVertical))
             && intent.hasExtra(getString(R.string.key_QtyHorizontal))
             && intent.hasExtra(getString(R.string.key_Color))
-            && intent.hasExtra(getString(R.string.key_Size))) {
+            && intent.hasExtra(getString(R.string.key_Size)))
+        {
             Size = intent.getIntExtra(getString(R.string.key_Size), 0);
             Color = intent.getIntExtra(getString(R.string.key_Color), 0);
             qty_H = intent.getIntExtra(getString(R.string.key_QtyHorizontal), 0);
             qty_V = intent.getIntExtra(getString(R.string.key_QtyVertical), 0);
         }
+
+        String s = String.format(getString(R.string.txtAllData), Size, Color, qty_H, qty_V);
+        txtViewAllData.setText(s);
     }
 }
