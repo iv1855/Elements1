49fb66cd50021cf35c5bf71117a08347c9fe4831
i <rp893@yandex.ru>
2021-03-06 20:53:42 +0300

    10 comm

--- app/src/main/java/com/example/elements1/ResultActivity.java
+++ app/src/main/java/com/example/elements1/ResultActivity.java
@@ -2,13 +2,32 @@ package com.example.elements1;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 public class ResultActivity extends AppCompatActivity {
+
+    private int Size;
+    private int Color;
+    private int qty_H;
+    private int qty_V;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_result);
+
+        Intent intent = getIntent();
+        if (intent.hasExtra(getString(R.string.key_QtyVertical))
+            && intent.hasExtra(getString(R.string.key_QtyHorizontal))
+            && intent.hasExtra(getString(R.string.key_Color))
+            && intent.hasExtra(getString(R.string.key_Size))) {
+            Size = intent.getIntExtra(getString(R.string.key_Size), 0);
+            Color = intent.getIntExtra(getString(R.string.key_Color), 0);
+            qty_H = intent.getIntExtra(getString(R.string.key_QtyHorizontal), 0);
+            qty_V = intent.getIntExtra(getString(R.string.key_QtyVertical), 0);
+        }
     }
 }
