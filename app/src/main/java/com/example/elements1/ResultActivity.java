3fcf7afc02e7d8041e998142bcf25646d44c9b8b
i <rp893@yandex.ru>
2021-04-02 17:13:28 +0300

    22 comm

--- app/src/main/java/com/example/elements1/ResultActivity.java
+++ app/src/main/java/com/example/elements1/ResultActivity.java
@@ -57,7 +57,8 @@ public class ResultActivity extends AppCompatActivity {
             public void onClick(View v) {
                 Context context = getApplicationContext();
 //                Toast.makeText(context, "Clicked!", Toast.LENGTH_SHORT).show();
-                makeAllElements(size, color, qty_H, qty_V);
+//                makeAllElements(size, color, qty_H, qty_V);
+                makeOneElement();
             }
         });
 
@@ -68,73 +69,60 @@ public class ResultActivity extends AppCompatActivity {
 
     private void makeAllElements(int size, int color, int qtyH, int qtyV) {
 
-        Toast.makeText(getApplicationContext(), "MakeElements!", Toast.LENGTH_SHORT).show();
-        Configuration configuration = this.getResources().getConfiguration();
-        int width = configuration.screenWidthDp;
-        int height = configuration.screenHeightDp;
-
-//        makeOneElement();
-
-        View[][] arrViews = new View[qtyH][qtyV];
-        Context context = getApplicationContext();
-        ConstraintSet constraintSet = new ConstraintSet();
-
-
-        for (int i = 0; i < qtyH; i++) {
-            for (int j = 0; j < qtyV; j++) {
-                View v = new View(context);
-                int currID = i * 100 + j;
-                v.setId(currID);
-                v.setBackgroundColor(color + currID * 100);
-                ViewGroup.LayoutParams layoutParams = new LayoutParams(size, size);
-                //LayoutParams layoutParams = v.getLayoutParams();
-//                layoutParams.width = size;
-//                layoutParams.height = size;
-                v.setLayoutParams(layoutParams);
-                arrViews[i][j] = v;
-                mainConstrLayout.addView(v);
-
-          }
-       }
-
-        //сделаем привязки
+        int colorWithAlfa = 0xFF000000 + color;
         ConstraintSet set = new ConstraintSet();
 
-        for (int i = 0; i <= arrViews.length; i++) {
-            for (int j = 0; j <= arrViews[0].length; j++) {
-                if (i == 0 && j == 0) {
-                    set.connect(arrViews[i][j].getId(), ConstraintSet.RIGHT, ConstraintSet.PARENT_ID, ConstraintSet.RIGHT);
+//        Creation
+        Button myBtn = new Button(this);
 
-                }
-
-
-
-
-            }
-        }
-
-
-
-//        set.connect(myBtn.getId(), ConstraintSet.LEFT,
-//                view3.getId(), ConstraintSet.RIGHT, 0);
-//        set.connect(myBtn.getId(), ConstraintSet.RIGHT,
-//                ConstraintSet.PARENT_ID, ConstraintSet.RIGHT, 0);
-//        set.connect(myBtn.getId(), ConstraintSet.TOP,
-//                ConstraintSet.PARENT_ID, ConstraintSet.TOP, 0);
-//        set.connect(myBtn.getId(), ConstraintSet.BOTTOM,
-//                ConstraintSet.PARENT_ID, ConstraintSet.BOTTOM, 0);
+//        SetProperties
+//        id
+        myBtn.setId(100 * 1 + 1);
+//        backgrColor
+        myBtn.setBackgroundColor(colorWithAlfa + 10000 * 1);
+//        text
+        myBtn.setText("1");
 
 
 
 
 
-
-
-        set.applyTo(mainConstrLayout);
-
-
-
-
+//        Toast.makeText(getApplicationContext(), "MakeElements!", Toast.LENGTH_SHORT).show();
+//        Configuration configuration = this.getResources().getConfiguration();
+//        int width = configuration.screenWidthDp;
+//        int height = configuration.screenHeightDp;
+//
+//        View[][] arrViews = new View[qtyH][qtyV];
+//        Context context = getApplicationContext();
+//        ConstraintSet constraintSet = new ConstraintSet();
+//
+//        for (int i = 0; i < qtyH; i++) {
+//            for (int j = 0; j < qtyV; j++) {
+//                View v = new View(context);
+//                int currID = i * 100 + j;
+//                v.setId(currID);
+//                v.setBackgroundColor(0xFF000000 + color + currID * 10000);
+//                ViewGroup.LayoutParams layoutParams = new LayoutParams(size, size);
+//                //LayoutParams layoutParams = v.getLayoutParams();
+////                layoutParams.width = size;
+////                layoutParams.height = size;
+//                v.setLayoutParams(layoutParams);
+//                arrViews[i][j] = v;
+//                mainConstrLayout.addView(v);
+//          }
+//       }
+//
+//        //сделаем привязки
+//        ConstraintSet set = new ConstraintSet();
+//
+//        for (int i = 0; i <= arrViews.length; i++) {
+//            for (int j = 0; j <= arrViews[0].length; j++) {
+//                if (i == 0 && j == 0) {
+//                    set.connect(arrViews[i][j].getId(), ConstraintSet.RIGHT, ConstraintSet.PARENT_ID, ConstraintSet.RIGHT);
+//                }
+//            }
+//        }
+//        set.applyTo(mainConstrLayout);
     }
 
     private void makeOneElement() {
@@ -161,10 +149,10 @@ public class ResultActivity extends AppCompatActivity {
 
         ConstraintSet set = new ConstraintSet();
         set.constrainHeight(myBtn.getId(), ConstraintSet.WRAP_CONTENT);
-//        set.constrainWidth(myBtn.getId(), ConstraintSet.WRAP_CONTENT);
+        set.constrainWidth(myBtn.getId(), ConstraintSet.WRAP_CONTENT);
 
         set.connect(myBtn.getId(), ConstraintSet.LEFT,
-                view3.getId(), ConstraintSet.RIGHT, 0);
+                ConstraintSet.PARENT_ID, ConstraintSet.LEFT, 0);
         set.connect(myBtn.getId(), ConstraintSet.RIGHT,
                 ConstraintSet.PARENT_ID, ConstraintSet.RIGHT, 0);
         set.connect(myBtn.getId(), ConstraintSet.TOP,
