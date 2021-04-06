faa00ca9a47c3d2d1b3f36eff3a9f577fc8385f9
i <rp893@yandex.ru>
2021-04-06 10:54:25 +0300

    27 comm

--- app/src/main/java/com/example/elements1/ResultActivity.java
+++ app/src/main/java/com/example/elements1/ResultActivity.java
@@ -118,8 +118,8 @@ public class ResultActivity extends AppCompatActivity {
             }
         }
 
-        for (int i = 0; i <= arrViews.length; i++) {
-            for (int j = 0; j <= arrViews[0].length; j++) {
+        for (int i = 0; i < arrViews.length; i++) {
+            for (int j = 0; j < arrViews[0].length; j++) {
                 if (arrViews[i][j] != null) {
                     if (i == 0 && j == 0) {
                         set.connect(arrViews[i][j].getId(), ConstraintSet.RIGHT, ConstraintSet.PARENT_ID, ConstraintSet.RIGHT);
